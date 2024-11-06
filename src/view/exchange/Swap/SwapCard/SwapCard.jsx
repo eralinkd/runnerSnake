3f2058dd81045a4e9f77e59bc82cd62b0c20c899
eralinkd <eralinkd@gmail.com>
2024-11-06 16:01:08 +0200

    swap page

--- src/view/exchange/Swap/SwapCard/SwapCard.jsx
+++ src/view/exchange/Swap/SwapCard/SwapCard.jsx
@@ -1,27 +1,87 @@
-import arrow from '../../../../assets/arrow-bottom.svg';
+import { useRef, useState } from 'react';
+
 import CurrencyCardInfo from '../../../../shared/CurrencyCardInfo/CurrencyCardInfo';
-// import clsx from 'clsx';
+import arrow from '../../../../assets/arrow-bottom.svg';
+import clsx from 'clsx';
 import styles from './SwapCard.module.scss';
+import { useOnClickOutside } from '../../../../hooks/useOnClickOutside';
 
 const SwapCard = ({ props }) => {
   const gradient = props.gradient;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const options = [
+    { value: '1', label: 'SCoin' },
+    { value: '2', label: 'TON' },
+    { value: '3', label: 'BitCoin' },
+    { value: '4', label: 'ScamCoin' },
+    { value: '5', label: 'NotScamCoin' },
+    { value: '6', label: 'KolyaHater' },
+    { value: '7', label: 'UlichniyDancer' },
+    { value: '8', label: '**' },
+    { value: '9', label: '***' },
+    { value: '10', label: '****' },
+  ]
+
+  const [selected, setOptionSelected] = useState(options.find((option) => option?.label === props.coin));
+
+  const selectRef = useRef(null);
+
+  const handleClose = () => setIsOpen(false);
+
+  useOnClickOutside(selectRef, handleClose);
+
+  const handleSelect = (option) => () => {
+    setOptionSelected(option);
+    handleClose();
+  };
+
 
   return (
     <article
       className={styles.article}
       style={{ '--border-gradient': gradient }}
     >
-      <p>dsds</p>
-      <div className={styles.currencyContainer}>
+      <div className={styles.balance}>
+        <p className='f-30'>0.<span className='f-23'>00</span></p>
+        <p className={clsx('f-10', styles.balanceText)}>0.00 {selected.label}</p>
+      </div>
+      <div className={clsx(styles.currencyContainer, isOpen && styles.opened)} ref={selectRef} onClick={() => setIsOpen(!isOpen)}>
         <CurrencyCardInfo
           className={styles.currencyInfo}
-          title={props.coin}
+          title={selected.label}
           imgSrc={props.coinImg}
           text={'Token'}
         ></CurrencyCardInfo>
+
+
+
         <button type="button" className={styles.moreActions}>
-          <img src={arrow} alt="icon dots" />
+          <img src={arrow} alt="icon arrow" />
         </button>
+
+        <ul className={styles.options}>
+          {options?.map((option) => {
+            console.log(selected)
+            const isSelected = selected.value === option?.value;
+
+            return (
+              <li
+                className={clsx(styles.option, isSelected && styles.isSelected)}
+                key={option.label}
+                onClick={handleSelect(option)}
+              >
+                <CurrencyCardInfo
+                  className={styles.currencyInfo}
+                  title={option.label}
+                  imgSrc={props.coinImg}
+                  text={'Token'}
+                ></CurrencyCardInfo>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </article>
   );
