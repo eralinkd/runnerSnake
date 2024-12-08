38e0aaff3be6647436916f71de2f196f78e97a9f
eralinkd <eralinkd@gmail.com>
2024-12-08 22:49:38 +0200

    profile final

--- src/view/profile/Profile.jsx
+++ src/view/profile/Profile.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from 'react';
 
 import SCoin from '../../assets/profile/snake.svg';
 import abu from '../../assets/temp/abu.png';
+import arrRight from '../../assets/taskArrowToRight.svg';
 import avatar from '../../assets/temp/avatar.svg';
 import clsx from 'clsx';
 import copy from '../../assets/profile/copy.svg';
@@ -10,9 +11,11 @@ import lightning from '../../assets/profile/lightning.svg';
 import person from '../../assets/person.svg';
 import snake from '../../assets/snake.svg';
 import styles from './Profile.module.scss';
+import taskImg from '../../assets/temp/task.svg';
 
 const Profile = () => {
   const [isCopied, setIsCopied] = useState(false);
+  const [activeTaskGroup, setActiveTaskGroup] = useState('main');
   const timeoutRef = useRef(null);
   const message = `dsfndshfns !!!`
   const shareLink = `https://t.me/share/url?url=t.me/snake_runner_dev_bot/snake_runner_dev?startapp=rp_1365932&text=${message}`;
@@ -28,11 +31,12 @@ const Profile = () => {
 
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
-      timeoutRef.current = setTimeout(() => {
-        setIsCopied(false);
-        timeoutRef.current = null;
-      }, 700);
     }
+
+    timeoutRef.current = setTimeout(() => {
+      setIsCopied(false);
+      timeoutRef.current = null;
+    }, 700);
   };
 
   return (
@@ -101,6 +105,80 @@ const Profile = () => {
 
           <div className={styles.emptyCard}>
             <p><img src={person} alt='person icon'></img>+</p>
+          </div>
+        </div>
+      </div>
+
+
+      <div className={styles.tasks}>
+        <h3>Задания</h3>
+        <ul>
+          <li
+            onClick={() => setActiveTaskGroup('main')}
+            className={activeTaskGroup === 'main' && styles.active}>
+            <span>Основные</span>
+          </li>
+          <li
+            onClick={() => setActiveTaskGroup('daily')}
+            className={clsx(styles.second, activeTaskGroup === 'daily' && styles.active)}>
+            <span className={styles.second}>Ежедневные</span>
+          </li>
+        </ul>
+
+        <div className={styles.tasksList}>
+          <div className={styles.task}>
+            <div className={styles.headingContainer}>
+              <p className={styles.heading}>
+                <img src={taskImg} alt='task img'></img>
+                Lorem ipsum dolor sit
+              </p>
+              <div className={styles.taskNavigate}>
+                <img src={arrRight} alt='arrow right'></img>
+              </div>
+            </div>
+            <div className={styles.info}>
+              <p className={styles.description}>Lorem ipsum dolor sit amet consectetur. Suspendisse placerat</p>
+              <p className={styles.reward}>
+                <img src={snake} alt='snake'></img>
+                5.000
+              </p>
+            </div>
+          </div>
+          <div className={styles.task}>
+            <div className={styles.headingContainer}>
+              <p className={styles.heading}>
+                <img src={taskImg} alt='task img'></img>
+                Lorem ipsum dolor sit
+              </p>
+              <div className={styles.taskNavigate}>
+                <img src={arrRight} alt='arrow right'></img>
+              </div>
+            </div>
+            <div className={styles.info}>
+              <p className={styles.description}>Lorem ipsum dolor sit amet consectetur. Suspendisse placerat</p>
+              <p className={styles.reward}>
+                <img src={snake} alt='snake'></img>
+                5.000
+              </p>
+            </div>
+          </div>
+          <div className={styles.task}>
+            <div className={styles.headingContainer}>
+              <p className={styles.heading}>
+                <img src={taskImg} alt='task img'></img>
+                Lorem ipsum dolor sit
+              </p>
+              <div className={styles.taskNavigate}>
+                <img src={arrRight} alt='arrow right'></img>
+              </div>
+            </div>
+            <div className={styles.info}>
+              <p className={styles.description}>Lorem ipsum dolor sit amet consectetur. Suspendisse placerat</p>
+              <p className={styles.reward}>
+                <img src={snake} alt='snake'></img>
+                5.000
+              </p>
+            </div>
           </div>
         </div>
       </div>
