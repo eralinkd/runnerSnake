ce1b00d96c38090b9453962b0aca0419850e2bc8
eralinkd <eralinkd@gmail.com>
2024-12-08 23:21:23 +0200

    final profile v2

--- src/shared/ComponentWithBorder/ComponentWithBorder.jsx
+++ src/shared/ComponentWithBorder/ComponentWithBorder.jsx
@@ -1,7 +1,8 @@
+import clsx from 'clsx';
 import styles from './ComponentWithBorder.module.scss';
 
-const GradientBorderWrapper = ({ children }) => {
-  return <div className={styles.gradientBorderWrapper}>{children}</div>;
+const GradientBorderWrapper = ({ children, className }) => {
+  return <div className={clsx(styles.gradientBorderWrapper, className)}>{children}</div>;
 };
 
 export default GradientBorderWrapper;
