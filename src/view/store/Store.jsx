471b7f1061b8e2598d73c2f7788258c843940e66
Богдан <sysoevbogdan82@gmail.com>
2024-12-09 04:13:24 +0200

    created store page

--- src/view/store/Store.jsx
+++ src/view/store/Store.jsx
@@ -1,5 +1,40 @@
+import StoreCard from './StoreCard/StoreCard';
+import testImage from '../../assets/store/tets.png';
+import styles from './Store.module.scss';
+import StoreModal from './StoreModal/StoreModal';
+
+const data = [
+  {
+    title: 'Шлем',
+    SCoinPrice: 123,
+    OtherPrice: 123,
+    imgSrc: testImage,
+  },
+  {
+    title: 'Кольчуга',
+    SCoinPrice: 123,
+    OtherPrice: 123,
+    imgSrc: testImage,
+  },
+  {
+    title: 'Кольчуга',
+    SCoinPrice: 123,
+    OtherPrice: 123,
+    imgSrc: testImage,
+  },
+];
+
 const Store = () => {
-  return <div>index</div>;
+  return (
+    <section className={styles.section}>
+      <ul className={styles.list}>
+        {data?.map((card) => (
+          <StoreCard key={card.title} card={card} />
+        ))}
+      </ul>
+      <StoreModal />
+    </section>
+  );
 };
 
 export default Store;
---
+++ src/view/store/StoreCard/StoreCard.jsx
@@ -0,0 +1,43 @@
+import ComponentWithBorder from '../../../shared/ComponentWithBorder/ComponentWithBorder.jsx';
+import snakeIcon from '../../../assets/snake.svg';
+// import usdtIcon from '../../../assets/usdt-white.svg';
+import arrow from '../../../assets/taskArrowToRight.svg';
+import styles from './StoreCard.module.scss';
+import storeModalState from '../../../state/StoreModalState.js';
+
+const StoreCard = ({ card }) => {
+  const { imgSrc, title } = card;
+  const openModal = storeModalState((state) => state.openModal);
+
+  return (
+    <ComponentWithBorder className={styles.cardWrapper}>
+      <article className={styles.card}>
+        <button
+          type="button"
+          onClick={() => openModal(title)}
+          className={styles.btn}
+        >
+          <img src={arrow} alt="arrow" className={styles.img} />
+        </button>
+        <div className={styles.imgContainer}>
+          <img src={imgSrc} alt="card" />
+        </div>
+        <div className={styles.content}>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.actions}>
+            <div className={styles.price}>
+              <img src={snakeIcon} alt="icon" />
+              <span className={styles.value}>123K</span>
+            </div>
+            {/* <div className={styles.price}>
+              <img src={usdtIcon} alt="icon" />
+              <span className={styles.value}>123K</span>
+            </div> */}
+          </div>
+        </div>
+      </article>
+    </ComponentWithBorder>
+  );
+};
+
+export default StoreCard;
