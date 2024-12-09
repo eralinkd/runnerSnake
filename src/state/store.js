d5aabd51ea701a03c4075c445531809dba3ed476
eralinkd <eralinkd@gmail.com>
2024-12-09 04:12:01 +0200

    prodile fixes

--- src/state/store.js
+++ src/state/store.js
@@ -3,6 +3,10 @@ import { create } from 'zustand'
 const useStore = create((set) => ({
     userId: 1,
     setUserId: (id) => set({ userId: id }),
+
+    userData: {},
+    setUserData: (data) => set({ userData: data }),
 }))
+
 
 export default useStore
