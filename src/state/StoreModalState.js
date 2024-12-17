9826fde0aaa5b319eabf22eda45354ddd154fa78
Богдан <sysoevbogdan82@gmail.com>
2024-12-17 18:44:22 +0200

    added prices store card

--- src/state/StoreModalState.js
+++ src/state/StoreModalState.js
@@ -2,9 +2,9 @@ import { create } from 'zustand'
 
 const storeModalState = create((set) => ({
 	isOpen: false,
-	modalData: { title: '' },
-	openModal: (title) =>
-		set({ isOpen: true, modalData: { title } }),
+	modalData: { title: '', prices: {}, id: null },
+	openModal: (title, prices, id) =>
+		set({ isOpen: true, modalData: { title, prices, id } }),
 	closeModal: () => set({ isOpen: false }),
 }));
 
