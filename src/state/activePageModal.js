8214598646a75f5dde08c0805fb04dcf8cddd5d7
eralinkd <eralinkd@gmail.com>
2024-12-18 21:56:59 +0200

    ?

--- src/state/activePageModal.js
+++ src/state/activePageModal.js
@@ -1,7 +1,9 @@
 import { create } from "zustand";
 import { menuItems } from "../constants/menuItems";
 
-export const useNavigationStore = create((set) => ({
+const useNavigationStore = create((set) => ({
 	activePage: menuItems[2].id,
 	setActivePage: (item) => set({ activePage: item }),
 }));
+
+export default useNavigationStore;
